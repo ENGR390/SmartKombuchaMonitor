5e9d09076f4128d0f5b5b164176f72c3991c892e
UlysseAll <u_allard@live.concordia.ca>
2025-10-10 16:39:05 -0400

    Created the early layout for new recipe page. In the next iteration I will make it so windows containing 1st fermentation, 2nd fermentation and so on colapse and open. We need to complete the set up of the data base to be able to save these reciepe and make multiple iteration.

--- app/src/main/java/com/example/kombuchaapp/MainActivity.java
+++ app/src/main/java/com/example/kombuchaapp/MainActivity.java
@@ -1,20 +1,34 @@
 package com.example.kombuchaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
 import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button appButton;
     private FirebaseAnalytics mFirebaseAnalytics;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
 
         // Initialize Firebase Analytics
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
@@ -29,6 +43,14 @@ public class MainActivity extends AppCompatActivity {
 
             // Log the button click event
             logButtonClickEvent();
+
+        });
+
+        // Settings button
+        appButton = findViewById(R.id.NewRecipeButton);
+        appButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, CreateANewRecipe.class);
+            startActivity(intent);
         });
     }
 
