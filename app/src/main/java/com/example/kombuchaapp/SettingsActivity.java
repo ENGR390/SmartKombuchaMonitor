7fd56aca18c79929c5998aaf0d1c69121175719c
Aaida Hossain <aaida.hossain1@gmail.com>
2025-11-22 14:32:40 -0500

    Update SettingsActivity.java

--- app/src/main/java/com/example/kombuchaapp/SettingsActivity.java
+++ app/src/main/java/com/example/kombuchaapp/SettingsActivity.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android:widget.ImageButton;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
