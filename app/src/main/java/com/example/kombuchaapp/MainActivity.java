ee353448661fc874cafb34541957174d109dd227
Aaida Hossain <aaida.hossain1@gmail.com>
2025-11-22 18:23:29 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/kombuchaapp/MainActivity.java
+++ app/src/main/java/com/example/kombuchaapp/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.kombuchaapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android:view.View;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
